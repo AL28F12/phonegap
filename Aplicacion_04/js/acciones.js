66a284205ffdae0efd9e15bf119b157552491985
Erick Yamil Popoca Ramos <erick97pr@gmail.com>
2014-05-30 09:14:09 -0500

    3
    tercer cambio

--- Aplicacion_04/js/acciones.js
+++ Aplicacion_04/js/acciones.js
@@ -13,10 +13,10 @@ $(document).ready(function(e) {
  document.addEventListener("deviceready",function(){
   $('#B_Vibrar').tap(function(){
     navigator.notification.vibrate(500);
-	$('#B_beep').tap(function(){
-	 navigator.notification.beep(2);	
   });
-   });
+   $('#B_Beep').tap(function(){
+    navigator.notification.beep(1);
+  });
  },false); //deviceready
 }); //document ready
 
